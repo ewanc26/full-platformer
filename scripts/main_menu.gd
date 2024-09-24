964ac5c356f13625b23012e912e31de3e931a9e0
Ewan <156911937+ewanc26@users.noreply.github.com>
2024-09-24 22:59:23 +0100

    update main menu with controller support

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -2,6 +2,10 @@ extends Control
 
 @onready var navigation: AudioStreamPlayer2D = $Audio/navigation
 @onready var click: AudioStreamPlayer2D = $Audio/click
+@onready var start_button: Button = $VBoxContainer/StartButton
+
+func _ready():
+	start_button.grab_focus()
 
 func _on_start_button_pressed() -> void:
 	$Audio/click.play()
@@ -11,5 +15,5 @@ func _on_quit_button_pressed() -> void:
 	$Audio/click.play()
 	get_tree().quit()
 	
-func _on_button_mouse_entered() -> void:
+func _on_button_focus_entered() -> void:
 	$Audio/navigation.play()
