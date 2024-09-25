475f7340edd68cfaf0e5f5e8ef54bd962ca07c7d
Ewan <156911937+ewanc26@users.noreply.github.com>
2024-09-25 02:15:14 +0100

    update the main menu

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,15 +1,18 @@
+class_name MainMenu
 extends Control
 
 @onready var start_button: Button = $Background/MarginContainer/VBoxContainer/StartButton
 
 func _on_start_button_pressed() -> void:
 	UISounds.play_click_sound()
-	get_tree().change_scene_to_file("res://menus/level_select.tscn")
-
+	LevelManager.load_level(1)
+	deactivate()
+	
 func _on_quit_button_pressed() -> void:
 	UISounds.play_click_sound()
+	await get_tree().create_timer(0.5).timeout  # Wait for 0.5 seconds
 	get_tree().quit()
-
+	
 func _on_button_focus_entered() -> void:
 	if not UILogic.is_using_mouse():  # Check if mouse is not used
 		UISounds.play_navigation_sound()
@@ -17,3 +20,17 @@ func _on_button_focus_entered() -> void:
 func _on_button_mouse_entered() -> void:
 	if not UILogic.is_using_mouse():  # Check if mouse is not used
 		UISounds.play_navigation_sound()
+
+func deactivate() -> void:
+	hide()
+	set_process(false)
+	set_physics_process(false)
+	set_process_unhandled_input(false)
+	set_process_input(false)
+
+func activate() -> void:
+	show()
+	set_process(true)
+	set_physics_process(true)
+	set_process_unhandled_input(true)
+	set_process_input(true)
